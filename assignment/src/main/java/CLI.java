2e0a41d898ccfcb6f8dd15aaf647b39498083138
dlsrks1021 <dlsrks1021@gmail.com>
2025-03-13 17:36:14 +0900

    feat: exit window if command contains exit kewords

--- assignment/src/main/java/CLI.java
+++ assignment/src/main/java/CLI.java
@@ -1,14 +1,22 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class CLI {
     private static final String PREFIX = "명령어 > ";
+    private static final String[] EXIT_KEYWORDS = {"exit", "종료"};
     private static final Scanner scanner = new Scanner(System.in);
 
     public static void openWindow() {
         while (true) {
             System.out.print(PREFIX);
             String input = scanner.nextLine();
-            System.out.println(input);
+
+            if (Arrays.asList(EXIT_KEYWORDS).contains(input)){
+                System.out.println("프로그램이 종료됩니다.");
+                break;
+            } else {
+                System.out.println("존재하지 않는 명령어 입니다.");
+            }
         }
     }
 }
