34c3e55a191e4b377375fabe8cf4d55179089092
dlsrks1021 <dlsrks1021@gmail.com>
2025-03-15 17:08:25 +0900

    feat: new command 'list' for print all posts (#16)

--- assignment/src/main/java/post/PostService.java
+++ assignment/src/main/java/post/PostService.java
@@ -2,6 +2,8 @@ package post;
 
 import lombok.RequiredArgsConstructor;
 
+import java.util.List;
+
 @RequiredArgsConstructor
 public class PostService {
     private final PostRepository postRepository;
@@ -11,12 +13,25 @@ public class PostService {
         postRepository.save(post);
     }
 
+    public String readAllPost() {
+        List<Post> posts = postRepository.findAll();
+        StringBuilder result = new StringBuilder();
+        String prefix = "총 게시글은 " + posts.size() + "개 작성되어 있습니다.\n";
+        result.append(prefix);
+        for (Post post : posts) {
+            result.append("\n");
+            result.append(readPost(post.getPostId()));
+        }
+
+        return result.toString();
+    }
+
     public String readPost(Long postId) {
         Post post = postRepository.find(postId);
         String result = "";
         result += postId + "번 게시물\n";
         result += "제목 : " + post.getPostTitle() + "\n";
-        result += "내용 : " + post.getPostContent();
+        result += "내용 : " + post.getPostContent() + "\n";
 
         return result;
     }
